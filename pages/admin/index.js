2dcc3a02ad300e38bbcd5f1b11945a0d996233b7
MaxKuch <narrm4517@gmail.com>
2021-11-13 20:14:38 +0300

    deployment to heroku

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,6 +1,6 @@
 import { useSelector } from "react-redux";
 import AdminPanel from "../../components/Admin/AdminPanel";
-import LoginForm from "../../components/Admin/Forms/LoginForm";
+import LoginForm from "../../components/Admin/forms/LoginForm";
 
 export default function Admin() {
     const isAuth = useSelector(state => state.auth.isAuth)
